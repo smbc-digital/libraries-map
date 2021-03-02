4125e69e4c0cefb126a8211d8156f13a608a7c6a
stephen.lally <stephen.lally.smbc@gmail.com>
2021-03-02 15:04:38 +0000

    feat(Style): updated map to ds

--- src/Popups/index.js
+++ src/Popups/index.js
@@ -1,12 +1,22 @@
 const librariesPopup = (feature, layer) => {
   
-  const content = `<div class="item"><i class="fa fa-book" aria-hidden="true"></i><p class="title">Library</p>
-  <p></p>
-  <p class="info"><a href="${feature.properties.url}" target="_blank">${feature.properties.facility_name}</a></p>
-  <p class="info">Address: ${feature.properties.postal_address}</p>
-  <p class="info"><a href="${feature.properties.opening__times}" target="_blank">Opening Times</a></p>
-  <p></p>
-  <p class="title"><a href="mailto:${feature.properties.email}" target="_blank">Email Library</a></p>
+  const content = `<div class="smbc-map__item">
+  <div class="smbc-map__item__header__block">
+    <i class="fa fa-book smbc-map__item__header__block__icon" aria-hidden="true"></i>
+    <span class="smbc-map__item__header__block__title">Library</span>
+  </div>
+  <div class="smbc-map__item__body">
+  <p class="smbc-body">
+    <a class="govuk-link" href="${feature.properties.url}" target="_blank">${feature.properties.facility_name}</a>
+  </p>
+  <p>Address: ${feature.properties.postal_address}</p>
+  <p class="smbc-body">
+    <a class="govuk-link" href="${feature.properties.opening__times}" target="_blank">Opening Times</a>
+  </p>
+  <p class="smbc-body">
+    <a class="govuk-link" href="mailto:${feature.properties.email}" target="_blank">Email Library</a>
+  </p>
+  </div>
   </div>`
 
   layer.bindPopup(content)
